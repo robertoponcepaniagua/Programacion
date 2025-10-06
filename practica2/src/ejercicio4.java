db8e6eb669761e1b0bb28b451068cedb29fbcef6
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-06 14:25:22 +0200

    Ejercicio4 en proceso

---
+++ practica2/src/ejercicio4.java
@@ -0,0 +1,12 @@
+public class ejercicio4 {
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+            }
+        for (int i = 1; i < 101; i++) {
+            int suma = a[i];
+            System.out.println(suma);
+        }
+    }
+}
