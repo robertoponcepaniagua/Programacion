126532af56f20f3c98e6c52f089fb6b39f1239a0
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-06 10:40:19 +0200

    Modificación .gitignore y ejercicio1 empezado

---
+++ practica2/src/ejercicio1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+    //Crea un array de 10 posiciones de números con valores pedidos
+    // por teclado. Muestra por consola el índice y el valor al que
+    // corresponde.
+    public static void main(String[] args) {
+        //Crea un array de 10 posiciones de números con valores pedidos por teclado
+        Scanner sc = new Scanner(System.in);
+        int[] arrayInt = new int[10];
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Introduce el siguiente valor: ");
+            arrayInt[i] = sc.nextInt();
+        }
+        //Muestra por consola el índice y el valor
+        //indice -> valor
+
+    }
+}
