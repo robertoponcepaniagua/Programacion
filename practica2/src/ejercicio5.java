b697312eae97c241b21b7cad5a568ecf27584c9a
roberto <robertoponcecl@gmail.com>
2025-10-15 13:32:43 +0200

    ejercicio 5 terminado ejercicio6 empezando

--- practica2/src/ejercicio5.java
+++ practica2/src/ejercicio5.java
@@ -7,16 +7,16 @@ public class ejercicio5 {
         System.out.print("Escribe el tamaño del array: ");
         int tamaño = sc.nextInt();
         int[] a = new int[tamaño];
-        int suma= 0;
+        int suma = 0;
         //rellenaremos el array con números aleatorios entre 0 y 9
-        for (int i=0; i<a.length;i++) {
-            a[i]= (int) (Math.random()*10);
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 10);
             suma += a[i];
         }
         //al final muestra por pantalla el
         //valor de cada posición y la suma de todos los valores.
-        for (int i = 0; i<a.length;i++) {
-            System.out.println("Indice: "+i+" : "+a[i]);
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Indice: " + i + " : " + a[i]);
         }
         System.out.println(suma);
     }
