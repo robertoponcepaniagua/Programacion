5e953b5f9f6db86c7c7354524749a1e82cd85a54
roberto <robertoponcecl@gmail.com>
2025-10-15 15:29:35 +0200

    ejercicio6 terminado

--- practica2/src/ejercicio6.java
+++ practica2/src/ejercicio6.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 
-public class ejercicio5 {
+public class ejercicio6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] a = new int[10];
+        int positivos = 0;
+        int negativos = 0;
+        int ceros = 0;
         for(int i = 0; i < 10; i++) {
-            System.out.println("Escribe 10 números: ");
+            System.out.println("Escribe el número " + (i + 1) + ": ");
             a[i]= sc.nextInt();
         }
+        for(int i = 0; i < 10; i++) {
+            if (a[i] > 0) {
+                positivos++;
+            }else if (a[i] < 0) {
+                negativos++;
+            }else {
+                ceros++;
+            }
+        }
+        System.out.println("Números positivos: " + positivos);
+        System.out.println("Números negativos: " + negativos);
+        System.out.println("Ceros: " + ceros);
     }
 }
