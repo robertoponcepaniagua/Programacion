b36a50e725005cd84c613931ed4c5992583f8a66
roberto <robertoponcecl@gmail.com>
2025-10-15 13:53:40 +0200

    ejercicio 6 emnpezado

--- practica2/src/ejercicio6.java
+++ practica2/src/ejercicio6.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class ejercicio5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] a = new int[10];
+        for(int i = 0; i < 10; i++) {
+            System.out.println("Escribe 10 números: ");
+            a[i]= sc.nextInt();
+        }
     }
 }
