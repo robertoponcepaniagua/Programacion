2ec3b68062fcd65e18a57276ebb4165c8ad253c5
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-20 12:32:26 +0200

    ejercicio10 reformulación de codigo, correción y revisión

--- practica2/src/ejercicio10.java
+++ practica2/src/ejercicio10.java
@@ -2,23 +2,37 @@ import java.util.Scanner;
 
 public class ejercicio10 {
     public static void main(String[] args) {
-        //Crea un programa que muestre por pantalla la nota de un estudiante, de entre una lista de estudiantes con sus respectivas notas.
-        // El nombre del estudiante lo introduce el usuario por teclado (usa la clase Scanner).
-        // Utiliza un array para los nombres de los alumnos y otro para las notas. ¿Serías capaz de hacerlo con un array BIdimensional?
         Scanner sc = new Scanner(System.in);
-        String[][] arraym = new String[20][2];
+        String[][] arraym = new String[5][2];
+
+        // Introducir nombres por teclado
         for (int i = 0; i < arraym.length; i++) {
             System.out.print("Rellena el nombre de los alumnos: ");
             arraym[i][0] = sc.nextLine();
         }
+
+        // Introducir notas por teclado
         for (int i = 0; i < arraym.length; i++) {
             System.out.print("Rellena la nota de los alumnos: ");
             int nota = sc.nextInt();
+            sc.nextLine();
             arraym[i][1] = String.valueOf(nota);
         }
-        for (int i = 0; i < arraym.length; i++) {
-            System.out.println("Nombre del alumno: "+arraym[i][0]+" su nota es= "+arraym[i][1]);
+        //buscar el alumno
+        System.out.println("Escribe el nombre de un alumno para ver su nota: ");
+        String NombreBuscado = sc.nextLine();
 
+        boolean encontrado = false;
+        for (int i = 0; i < arraym.length; i++) {
+            if (arraym[i][0].equalsIgnoreCase(NombreBuscado)) {
+                System.out.println(arraym[i][0] + " tiene de nota un: " + arraym[i][1]);
+                encontrado = true;
+                break;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Alumno no encontrado");
         }
     }
 }
