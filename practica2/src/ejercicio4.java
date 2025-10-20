6806717402916973da70a106c60bc839b472e438
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-20 12:15:23 +0200

    ejercicio4 actualizado

--- practica2/src/ejercicio4.java
+++ practica2/src/ejercicio4.java
@@ -2,10 +2,10 @@ public class ejercicio4 {
     public static void main(String[] args) {
         int[] a = new int[100];
         int suma =0;
-        for (int i = 1; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             a[i] = i + 1;
             }
-        for (int i = 1; i < 100; i++) {
+        for (int i = 0; i < 100; i++) {
             suma += a[i];
         }
         double media = (double) suma / a.length;
