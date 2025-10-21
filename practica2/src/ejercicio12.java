69253d6d8e6ecf2a3c89fca2c52edf1dcd8491d6
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-21 11:59:39 +0200

    ejercicio 11 y 12 empezado

---
+++ practica2/src/ejercicio12.java
@@ -0,0 +1,10 @@
+public class ejercicio12 {
+    public static void main(String[] args) {
+    //En España cada persona está identificada con un Documento Nacional de Identidad (DNI) en
+        //el que figura un número y una letra, por ejemplo 56999545W. Realiza un programa donde le
+        //pidas al usuario SOLO el número del dni y el programa te devuelva la letra. Para calcular la
+        //letra solo tienes que dividir el número del DNI entre 23, el resto de esta división se
+        //corresponde con la posición de la letra en el abecedario. Utiliza un array para guardar CADA
+        //letra del abecedario.
+    }
+}
