73d712014c1031a3a26d04b74bebde0a2da6f68b
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-22 11:41:03 +0200

    Ejercicio 12 terminado

--- practica2/src/ejercicio12.java
+++ practica2/src/ejercicio12.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ejercicio12 {
     public static void main(String[] args) {
     //En España cada persona está identificada con un Documento Nacional de Identidad (DNI) en
@@ -6,5 +8,12 @@ public class ejercicio12 {
         //letra solo tienes que dividir el número del DNI entre 23, el resto de esta división se
         //corresponde con la posición de la letra en el abecedario. Utiliza un array para guardar CADA
         //letra del abecedario.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Escribe los números del DNI: "); //Pedimos el número del DNI
+        int Dni = sc.nextInt();
+        int resto =0;
+        char[] letrasDni = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'};
+        resto = Dni % 23;
+        System.out.println("Tú DNI es: "+Dni+letrasDni[resto]);
     }
 }
