f73088f337b377acbc5c31909835492325f17a6f
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2025-10-27 13:21:24 +0100

    Creación de rama practica3, carpeta y ejercicio1

---
+++ practica3/src/ejercicio1.java
@@ -0,0 +1,8 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+        //Define un array de números tipo double de 3 filas por 7 columnas con nombre doub y
+        //asigna los valores según la siguiente tabla. Muestra el contenido de todos los elementos del
+        //array dispuestos en forma de tabla como se muestra en la figura.
+        double[][]
+    }
+}
