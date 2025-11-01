27c832665d2620e2272b2878d8c68e7406869d87
roberto <robertoponcecl@gmail.com>
2025-11-01 19:20:55 +0100

    ejercicio 2 terminado, falta explicar la sintaxis del código

--- practica3/src/ejercicio2.java
+++ practica3/src/ejercicio2.java
@@ -1,3 +1,4 @@
+import java.util.Formattable;
 import java.util.Scanner;
 
 public class ejercicio2 {
@@ -10,6 +11,8 @@ public class ejercicio2 {
         //Escribe un programa que solicite 20 números enteros
         Scanner sc = new Scanner(System.in);
         int[][] array = new int[4][5];
+        int suma_total = 0;
+        int[] max = new int[5];
         // Estos números debemos de introducirlo en un array de 4 filas por 5 columnas.
         System.out.println("Escribe números para el array 4x5: ");
         for (int i = 0; i < 4; i++) {
@@ -18,7 +21,32 @@ public class ejercicio2 {
                 array[i][j] = sc.nextInt();
             }
         }
+        for (int j = 0; j < 5; j++) {
+            max[j] = array[0][j];
+        }
+        int[] sumacolumna = new int[5];
         // El programa mostrará las sumas parciales de filas y en las columnas el mayor número de la columna.
-
+        for (int i = 0; i < 4; i++) {
+            int sumafila = 0;
+            for (int j = 0; j < 5; j++) {
+                System.out.printf("%-5d", array[i][j]);
+                sumafila += array[i][j];
+                sumacolumna[j] += array[i][j];
+                suma_total += array[i][j];
+                if (array[i][j] > max[j]) {
+                    max[j] = array[i][j];
+                }
+            }
+            System.out.printf(" | %-5d\n", sumafila);
+        }
+        System.out.println("--------------------------------");
+        for (int j = 0; j < 5; j++) {
+            System.out.printf("%-5d", sumacolumna[j]);
+        }
+        System.out.printf("| %5d",suma_total);
+        System.out.println();
+        for (int j = 0; j < 5; j++) {
+            System.out.println("El número más grande de la columna " + j + " es: " + max[j]);
+        }
     }
 }
