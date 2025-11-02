8f4950e34a6b5c093918071718165ad6f67efe7f
roberto <robertoponcecl@gmail.com>
2025-11-02 17:58:19 +0100

    ejercicio 5 empezado, arreglo de fallos y siguiendo el código

--- practica3/src/ejercicio5.java
+++ practica3/src/ejercicio5.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class ejercicio5 {
     public static void main(String[] args) {
@@ -9,46 +8,60 @@ public class ejercicio5 {
         //- la suma total de todas las filas y columnas
         //- la suma de todas las columnas
         //- la suma de todas las filas.
-        Scanner sc = new Scanner(System.in);
+
         int[][] a = new int[6][10];
-        int menor = 0;
-        int mayor = 1000;
+
+        //suma total
         int sumatotal= 0;
+
+        //suma columnas y filas
         int[] columnas = new int[10];
         int[] filas = new int[6];
+
+        int menor = 0;
+        int mayor = 1000;
+
 
         //Rellenamos eñ array con números enteros positivos comprendidos entre 0 y 1000
 
         for (int i = 0; i <a.length; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print("Escribe un número positivo entre 0 y 1000: ");
-                a[i][j] = sc.nextInt();
-                while ((a[i][j] <= menor) || (a[i][j] >= mayor)) {
-                    System.out.print("Escribe otro número, recuerda que debe ser números enteros positivos entre 0 y 1000: ");
-                    a[i][j] = sc.nextInt();
-                }
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = (int) (Math.random() * 1001);
+
+                //sumas
                 sumatotal += a[i][j];
                 filas[i] = a[i][j];
                 columnas[j] += a[i][j];
+
             }
         }
 
-        //Número min y max
-
+        //max y min
         int max = a[0][0];
+        int filamax = 0;
+        int columnamax= 0;
         int min = a[0][0];
+        int filamin = 0;
+        int columnamin= 0;
 
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i <a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                //max y min
                 if (a[i][j] < min) {
                     min = a[i][j];
-                } else if (a[i][j] > max) {
+                    filamin = i;
+                    columnamin = j;
+                }
+                if (a[i][j] > max) {
                     max = a[i][j];
+                    filamax = i;
+                    columnamax = j;
                 }
             }
         }
-        System.out.println("El número maximo es ["+max+"]");
-        System.out.println("El número mínimo es ["+min+"]");
+        //- dar la posición del número máximo y mínimo
+        System.out.println("El número maximo es ["+max+"] en la posición ["+filamax+"]["+columnamax+"]");
+        System.out.println("El número mínimo es ["+min+"] en la posición ["+filamin+"]["+columnamin+"]");
 
     }
 }
