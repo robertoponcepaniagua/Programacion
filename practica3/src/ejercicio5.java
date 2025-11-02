215276eb2a6b90284e568e485b096eaeb1fd9f33
roberto <robertoponcecl@gmail.com>
2025-11-02 20:54:36 +0100

    ejercicio 5 terminado

--- practica3/src/ejercicio5.java
+++ practica3/src/ejercicio5.java
@@ -1,4 +1,3 @@
-
 public class ejercicio5 {
     public static void main(String[] args) {
         //Crear un programa que cuando se le introduzca números enteros rellene un array de 6 filas
@@ -21,19 +20,26 @@ public class ejercicio5 {
         int menor = 0;
         int mayor = 1000;
 
-
         //Rellenamos eñ array con números enteros positivos comprendidos entre 0 y 1000
-
         for (int i = 0; i <a.length; i++) {
             for (int j = 0; j < a[i].length; j++) {
                 a[i][j] = (int) (Math.random() * 1001);
 
                 //sumas
                 sumatotal += a[i][j];
-                filas[i] = a[i][j];
+                filas[i] += a[i][j];
                 columnas[j] += a[i][j];
-
             }
+        }
+
+        // Imprimir la tabla
+        System.out.println("Tabla generada:");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print("Fila " + i + ": ");
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%5d ", a[i][j]);
+            }
+            System.out.println();
         }
 
         //max y min
@@ -59,9 +65,23 @@ public class ejercicio5 {
                 }
             }
         }
-        //- dar la posición del número máximo y mínimo
+
+        //posición del número máximo y mínimo
         System.out.println("El número maximo es ["+max+"] en la posición ["+filamax+"]["+columnamax+"]");
         System.out.println("El número mínimo es ["+min+"] en la posición ["+filamin+"]["+columnamin+"]");
 
+        //Suma total
+        System.out.println("La suma total "+sumatotal);
+
+        //Columnas y filas
+        System.out.println("Suma por columnas:");
+        for (int j = 0; j < columnas.length; j++) {
+            System.out.println("Columna " + j + ": " + columnas[j]);
+        }
+
+        System.out.println("Suma por filas:");
+        for (int i = 0; i < filas.length; i++) {
+            System.out.println("Fila " + i + ": " + filas[i]);
+        }
     }
 }
