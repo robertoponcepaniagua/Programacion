b68b6626c97fc036cd42949044e542f99832abcb
roberto <robertoponcecl@gmail.com>
2025-11-18 21:20:06 +0100

    falta hacer el check usuario que tengo una duda

--- Ejercicio_POO/src/Main.java
+++ Ejercicio_POO/src/Main.java
@@ -17,13 +17,23 @@ public class Main {
         Usuario usuario3= new Usuario("Nay","Garrido Barbadilla","28485","Calle Alameda","naynay@hola","nayhola1234");
         Usuario[] usuarios = {usuario1,usuario2,usuario3};
         validarRegistro(usuarios);
+        checkusuario(usuarios);
     }
     public static void validarRegistro(Usuario[] usuarios){
         for (int i = 0; i < usuarios.length; i++) {
             if (usuarios[i].getCodigopostal().startsWith("28")) {
                 System.out.println("Codigo postal Valido");
             } else {
-                System.out.println("Código Postal no valido");
+                System.out.println("ERROR: "+usuarios[i].getNombre()+" bloqueado. Código Postal no permitido.");
+            }
+        }
+    }
+    public static void checkusuario(Usuario[] usuarios) {
+        for (int i = 0; i < usuarios.length; i++) {
+            if (usuarios[i].checkusuario(//falta esto)) {
+                System.out.println("Acceso concedido a "+usuarios[i].getNombre());
+            } else {
+                System.out.print("¡ERROR! Contraseña incorrecta para "+usuarios[i].getNombre());
             }
         }
     }
