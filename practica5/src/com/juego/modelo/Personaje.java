443a82a941c35c2924abe3d724d9df6d7fa11b71
roberto <robertoponcecl@gmail.com>
2026-01-06 19:11:23 +0100

    Creación de Personajes

---
+++ practica5/src/com/juego/modelo/Personaje.java
@@ -0,0 +1,137 @@
+package com.juego.modelo;
+
+import com.juego.clases.Clase;
+import com.juego.habilidades.Habilidad;
+import com.juego.razas.Raza;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Personaje {
+    private String nombre;
+    //Vida
+    private int vida;
+    private int vidaMax;
+    //Fuerza
+    private int fuerza;
+    //Defensa
+    private int defensa;
+    //Inteligencia
+    private int inteligencia;
+    //Destreza
+    private int destreza;
+    //Velocidad
+    private int velocidad;
+
+    private Clase clase;
+    private Raza raza;
+
+    public Personaje (String nombre, Clase clase, Raza raza) {
+        this.nombre = nombre;
+        this.clase = clase;
+        this.raza = raza;
+
+
+    }
+    //GET Y SET
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public Clase getClase() {
+        return clase;
+    }
+
+    public void setClase(Clase clase) {
+        this.clase = clase;
+    }
+
+    public Raza getRaza() {
+        return raza;
+    }
+
+    public void setRaza(Raza raza) {
+        this.raza = raza;
+    }
+
+
+    public int getVidaMax() {
+        return vidaMax;
+    }
+
+    public void setVidaMax(int vidaMax) {
+        this.vidaMax = vidaMax;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(int velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public void Corazones() {
+        int MaxCorazones = vidaMax / 10; // Calculo cuantos corazones max tiene "♥"
+        int corazones = vida / (vidaMax / MaxCorazones); // bloques llenos dependiendo de la vida actual
+        int Corazonesvacios = MaxCorazones - corazones; // Corazones Vacios
+
+        //LLENO
+        for (int i = 0; i < corazones ; i++) {
+            System.out.print("♥");
+        }
+
+        //VACIO
+        for (int i = 0; i < Corazonesvacios ; i++) {
+            System.out.print("♡");
+        }
+    }
+    public void atacar (Personaje pj) {
+        
+    }
+}
