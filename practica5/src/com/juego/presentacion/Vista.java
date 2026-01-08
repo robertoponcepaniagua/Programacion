473ed5850979667a695e9bf3da3eb2bb14e3e3a0
roberto <robertoponcecl@gmail.com>
2026-01-08 22:04:09 +0100

    Implementación/Prueba Vista con elegirHabilidad

--- practica5/src/com/juego/presentacion/Vista.java
+++ practica5/src/com/juego/presentacion/Vista.java
@@ -1,4 +1,23 @@
 package com.juego.presentacion;
 
+import com.juego.clases.Clase;
+import com.juego.habilidades.Habilidad;
+import com.juego.modelo.Personaje;
+
+import java.util.Scanner;
+
 public class Vista {
+    Scanner sc = new Scanner(System.in);
+    public Habilidad elegirHabilidad(Personaje pj) {
+        //Se crea una clase que coge la clase del personaje seleccionado.
+        Clase clasepj = pj.getClase();
+        for (int i = 0; i < clasepj.getHabilidades().size(); i++) {
+            System.out.print(i+". - ");
+            clasepj.getHabilidades().get(i).infoHabilidad();
+            System.out.println();
+            System.out.println();
+        }
+        int opcion = sc.nextInt();
+        return null;
+    }
 }
