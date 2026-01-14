04c4d6f662a38c08449adc2b10815733198b88a7
roberto <robertoponcecl@gmail.com>
2026-01-15 00:02:14 +0100

    Pruebas Main

--- practica5/src/Main.java
+++ practica5/src/Main.java
@@ -13,5 +13,11 @@ public class Main {
 
         Personaje pj1 = gestor.crearpj();
         System.out.println();
+
+        // PRUEBA DE VIDA, HAY QUE PROBAR LAS CURAS
+        pj1.corazones();
+        System.out.println();
+        pj1.setVida(45);
+        pj1.corazones();
     }
 }
