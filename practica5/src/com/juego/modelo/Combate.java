aefb491e718900251e7aef275908073043e852e4
roberto <robertoponcecl@gmail.com>
2026-01-15 00:03:02 +0100

    Implementación de metodos para el metodo combate

--- practica5/src/com/juego/modelo/Combate.java
+++ practica5/src/com/juego/modelo/Combate.java
@@ -1,6 +1,7 @@
 package com.juego.modelo;
 
 import com.juego.clases.Clase;
+import com.juego.habilidades.DanioCC;
 import com.juego.habilidades.Habilidad;
 import com.juego.habilidades.IHabilidad;
 import com.juego.presentacion.Vista;
@@ -11,11 +12,57 @@ import java.util.Scanner;
 public class Combate {
 
     //metodo para saber quien tiene más velocidad, quien tenga más velocidad ataca antes
-    public void atacaPrimero(Personaje p1, Personaje p2) {
+    public Personaje masRapido(Personaje p1, Personaje p2) {
         if (p1.getVelocidad() > p2.getVelocidad()) {
-            p1.atacar(p2);
+            return p1;
+        } else if (p2.getVelocidad() > p1.getVelocidad()) {
+            return p2;
         } else {
-            p2.atacar(p1);
+            // Si tienen misma velocidad se decide random
+            if (Math.random() < 0.5) {
+                return p1;
+            } else {
+                return p2;
+            }
+        }
+    }
+
+
+
+    //-------------------------------------COMBATE-------------------------------
+    public void combate(Personaje pj1 ,Personaje pj2) {
+
+        Personaje primero = masRapido(pj1, pj2);
+        Personaje segundo = null; //Inicializamos con null
+
+        if (primero == pj1) { //si primero es pj1
+        segundo = pj2;  //segundo es pj2
+        } else {  //si no
+            segundo = pj1; // segundo es pj1
+        }
+
+        //Mientras los 2 estén -----VIVOS---- sigue el combate, si uno de ellos muere termina.
+        while (pj1.getVida() > 0 && pj2.getVida() > 0) {
+
+            //Ataca el que más velocidad tiene que es primero
+
+        }
+    }
+    public void atacar(Personaje atacante, Personaje defensor) {
+        Vista vista = new Vista();
+
+        IHabilidad habilidadElegida = vista.elegirHabilidad(atacante);
+        habilidadElegida.usar();
+
+        int bonus = habilidadElegida.escaladoPoder(atacante);
+
+        switch (habilidadElegida.getTipo()) {
+            case "DanioCC":
+            case "DanioLD":
+            defensor.recibirAtaque(habilidadElegida , bonus);
+
+            //IMPLEMENTAR INFORMACIÓN DE LOS ATAQUES, PODER CUANTO QUITA ETC....
+            System.out.println(atacante.getNombre() + "usa "+ habilidadElegida.nombreHabilidad() + "");
         }
     }
 }
