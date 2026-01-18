b85a3e85eeed2f719f7909cefbce758636bacb38
roberto <robertoponcecl@gmail.com>
2026-01-18 14:24:08 +0100

    TERMINADO

--- practica5/src/com/juego/presentacion/Vista.java
+++ practica5/src/com/juego/presentacion/Vista.java
@@ -4,6 +4,7 @@ import com.juego.clases.Clase;
 import com.juego.habilidades.Habilidad;
 import com.juego.habilidades.IHabilidad;
 import com.juego.modelo.Personaje;
+import com.juego.modelo.PrecargaDatos;
 
 import java.util.List;
 import java.util.Scanner;
@@ -11,42 +12,37 @@ import java.util.Scanner;
 public class Vista {
     //meotod para elegir la habilidad que vas a utilizar contra el contrincante
     public IHabilidad elegirHabilidad(Personaje pj) {
-
+        List<IHabilidad> habilidades = pj.getClase().getHabilidades();
         Scanner sc = new Scanner(System.in);
-
-        for (int i = 0; i < pj.getClase().getHabilidades().size(); i++) {
-            System.out.println("┌──────────────────────────────────────────── HABILIDAD " + (i+1) +" ────────────────────────────────────────────┐");
-            pj.getClase().getHabilidades().get(i).infoHabilidad();
-            System.out.println("└──────────────────────────────────────────────── ⋆⋅☆⋅⋆ ───────────────────────────────────────────────┘");
-
-        }
-        System.out.println("Qué habilidad quieres usar?");
-
-        int opcion = sc.nextInt();
-
         IHabilidad habilidadElegida = null;
 
-        List<IHabilidad> habilidades = pj.getClase().getHabilidades();
         do {
-            switch (opcion) {
-                case 1:
-                    habilidadElegida = habilidades.get(0);
-                    break;
-                case 2:
-                    habilidadElegida = habilidades.get(1);
-                    break;
-                case 3:
-                    habilidadElegida = habilidades.get(2);
-                    break;
-                default:
-                    System.out.print("Opción no valida");
+            // Mostrar habilidades
+            for (int i = 0; i < habilidades.size(); i++) {
+                System.out.println("┌───────────────────────────── HABILIDAD " + (i+1) + " ─────────────────────────────┐");
+                habilidades.get(i).infoHabilidad();
+                System.out.println("└───────────────────────────────────────────────────────────────────────┘");
             }
+
+            System.out.println("Qué habilidad quieres usar?");
+            int opcion = sc.nextInt();
+
+            if (opcion >= 1 && opcion <= habilidades.size()) {
+                habilidadElegida = habilidades.get(opcion - 1);
+                if (habilidadElegida.getUsosActuales() <= 0) {
+                    System.out.println("Esa habilidad no tiene usos, elige otra.");
+                    habilidadElegida = null; // fuerza a repetir
+                }
+            } else {
+                System.out.println("Opción no válida, intenta de nuevo.");
+            }
+
         } while (habilidadElegida == null);
 
-        System.out.print("Has elegido: ");
-        System.out.println(habilidadElegida.nombreHabilidad());
+        System.out.println("Has elegido: " + habilidadElegida.nombreHabilidad());
         return habilidadElegida;
     }
+
 
 
     public void corazones(Personaje pj) {
@@ -61,10 +57,6 @@ public class Vista {
             System.out.print("♡");
         }
         System.out.print("   HP: " + pj.getVida() + "/" + pj.getVidaMax());
-    }
-
-    public void victoria(Personaje pj1) {
-        //HACER!!
     }
 
     public void mostrarEstado(Personaje p1, Personaje p2) {
@@ -84,6 +76,53 @@ public class Vista {
 
         System.out.println();
         System.out.println("╚══❖═════════════════════════❖══╝");
+        System.out.println();
+    }
+    public Personaje elegirPJ(PrecargaDatos precargaDatos) {
+
+        Personaje personaje = null;
+
+        do {
+            for (int i = 0; i < precargaDatos.precargaPJ().size(); i++) {
+                System.out.println("        ──────────────────────────────" + i + "──────────────────────────────       ");
+                infoPJ(precargaDatos.getPersonajes().get(i));
+                System.out.println();
+            }
+
+            System.out.println("Qué personaje quieres elegir?");
+            Scanner sc = new Scanner(System.in);
+            int opcion = sc.nextInt();
+
+            personaje = precargaDatos.getPersonajes().get(opcion);
+
+        }while (personaje == null);
+        return personaje;
+    }
+
+    public void infoPJ(Personaje pj) {
+
+        System.out.println();
+        System.out.printf("%-20s", "Nombre");
+        System.out.printf("%-15s", "Raza");
+        System.out.printf("%-15s", "Clase");
+        System.out.printf("%-10s", "Fuerza");
+        System.out.printf("%-15s", "Inteligencia");
+        System.out.printf("%-12s", "Velocidad");
+        System.out.printf("%-12s", "Destreza");
+        System.out.printf("%-12s", "Defensa");
+        System.out.printf("%-15s%n", "Vida");
+
+        System.out.println();
+        System.out.printf("%-20s", pj.getNombre());
+        System.out.printf("%-15s", pj.getRaza().getClass().getSimpleName());
+        System.out.printf("%-15s", pj.getClase().getClass().getSimpleName());
+        System.out.printf("%-10d", pj.getFuerza());
+        System.out.printf("%-15d", pj.getInteligencia());
+        System.out.printf("%-12d", pj.getVelocidad());
+        System.out.printf("%-12d", pj.getDestreza());
+        System.out.printf("%-12d", pj.getDefensa());
+
+        corazones(pj);
         System.out.println();
     }
 }
