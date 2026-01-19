d6fcfa5ee592669c3bf87d3547d8bfeec97d66f1
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2026-01-19 14:07:54 +0100

    MAIN FINALIZADO

--- practica5/src/Main.java
+++ practica5/src/Main.java
@@ -3,6 +3,5 @@ import com.juego.presentacion.Presentador;
 public class Main {
     public static void main(String [] args) {
         Presentador presentador = new Presentador();
-        presentador.menu();
     }
 }
