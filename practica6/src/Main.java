fe905e3b8afa7fce5d588be0bcd4866ef001d109
roberto <robertoponcecl@gmail.com>
2026-02-09 19:53:35 +0100

    PRUEBAS MAIN

--- practica6/src/Main.java
+++ practica6/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public void main (String []args) {
-        Bloque1 b1 = new Bloque1();
-        Bloque2 b2 = new Bloque2();
+
         Bloque3 b3 = new Bloque3();
+        b3.mensageriaGlobal();
     }
 }
