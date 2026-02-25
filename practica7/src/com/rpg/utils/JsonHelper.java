0666e02f2ecc7f236652aa70b95c02f41cd91b99
Roberto Ponce Paniagua <robertoponcecl@gmail.com>
2026-02-25 14:27:48 +0100

    Trabajo clase

--- practica7/src/com/rpg/utils/JsonHelper.java
+++ practica7/src/com/rpg/utils/JsonHelper.java
@@ -1,4 +1,32 @@
 package com.rpg.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.rpg.model.Ciudad;
+import com.rpg.model.Item;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class JsonHelper {
+    //ATRIBUTOS
+    private Reader reader;
+    private List<Item> lista;
+
+    public JsonHelper() throws IOException {
+        //CREAMOS EL LECTOR DEL ARCHIVO
+        reader = Files.newBufferedReader(Paths.get("practica7/Ficheros/ciudades.json"));
+
+        //USAMOS TYPETOKEN PARA DECIRLE A GSON QUE QUEREMOS UNA LISTA DE CIUDADES   (CUIDADO ES UNA LISTA)
+        lista = new Gson().fromJson(reader, new TypeToken<List<Ciudad>>(){}.getType());
+
+        //IMPRIMIMOS PARA COMPROBAR
+        for (Ciudad c : lista) {
+            //Seguir
+            System.out.print("Nombre Ciudad: ");
+        }
+    }
 }
--- practica7/src/com/rpg/utils/TxtHelper.java
+++ practica7/src/com/rpg/utils/TxtHelper.java
@@ -1,4 +1,43 @@
 package com.rpg.utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TxtHelper {
+    private File file;
+    private FileReader fr;
+    private BufferedReader br;
+
+    public TxtHelper() throws IOException {
+        this.file = new File("practica7/Ficheros/ciudades.txt"); //para representar el fichero que se quiere leer.
+        this.fr = new FileReader(file);  //para poder leer un fichero
+        this.br = new BufferedReader(fr);  // para leer más de un caracter
+    }
+
+    public void leerLineas() throws IOException {
+        String linea = br.readLine();
+        while (linea != null) {
+            System.out.print(linea);
+            linea = br.readLine();
+        }
+        fr.close();
+    }
+
+    // TODO: ELEGIR UNA DE LOS 2
+
+    //public void leerLineas2() {
+    //    try {
+    //        var file = new BufferedReader(new FileReader("practica7/Ficheros/ciudades.txt"));
+
+    //        String linea;
+    //        while ((linea = file.readLine()) != null)
+    //            System.out.println(linea);
+
+    //        file.close();
+    //    } catch (IOException e) {
+    //        System.out.println("No se ha podido abrir el fichero.");
+    //    }
+    //}
 }
