7fd74f2a19015ffffed164f5e8aadb6517e1213c
roberto <robertoponcecl@gmail.com>
2026-02-25 19:07:55 +0100

    Items clase creada

--- practica7/src/com/rpg/model/Item.java
+++ practica7/src/com/rpg/model/Item.java
@@ -5,4 +5,43 @@ public class Item {
     private String nombre;
     private String tipo;
     private int valor;
+
+    public Item(int id, String nombre, String tipo, int valor) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
 }
