01e7fb1faf990a6a3f29b77a065b368def6d6356
roberto <robertoponcecl@gmail.com>
2026-02-25 19:07:45 +0100

    JsonHelper empezado

--- practica7/src/com/rpg/utils/JsonHelper.java
+++ practica7/src/com/rpg/utils/JsonHelper.java
@@ -5,28 +5,26 @@ import com.google.gson.reflect.TypeToken;
 import com.rpg.model.Ciudad;
 import com.rpg.model.Item;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class JsonHelper {
-    //ATRIBUTOS
-    private Reader reader;
-    private List<Item> lista;
+    public JsonHelper() {
+    }
 
-    public JsonHelper() throws IOException {
-        //CREAMOS EL LECTOR DEL ARCHIVO
-        reader = Files.newBufferedReader(Paths.get("practica7/Ficheros/ciudades.json"));
-
-        //USAMOS TYPETOKEN PARA DECIRLE A GSON QUE QUEREMOS UNA LISTA DE CIUDADES   (CUIDADO ES UNA LISTA)
-        lista = new Gson().fromJson(reader, new TypeToken<List<Ciudad>>(){}.getType());
-
-        //IMPRIMIMOS PARA COMPROBAR
-        for (Ciudad c : lista) {
-            //Seguir
-            System.out.print("Nombre Ciudad: ");
+    public <T> List<T> readlist(String path, Class<T> tClass){
+        try (Reader reader = new FileReader(path)){
+            Type typetoken = TypeToken.getParameterized(List.class, tClass).getType();
+            Gson gson = new Gson();
+            return  gson.fromJson(reader, typetoken);
+        } catch (IOException e){
+            System.out.println("No se ha encontrado el archivo");
+            return List.of();
         }
     }
 }
