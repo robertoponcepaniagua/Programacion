8503c6ecbe9ef3c980890531f9de25bedcb6ab55
roberto <robertoponcecl@gmail.com>
2026-03-03 22:41:29 +0100

    Empiezo de Logger custom

--- practica7/src/com/rpg/utils/LoggerCustom.java
+++ practica7/src/com/rpg/utils/LoggerCustom.java
@@ -1,4 +1,12 @@
 package com.rpg.utils;
 
 public class LoggerCustom {
+
+
+    // EL FORMATO TIENE QUE SER [FECHA] [TIPO (INFO O ERROR) ] [PAQUETE] MENSAJE
+    public LoggerCustom() {
+
+    }
+
+
 }
