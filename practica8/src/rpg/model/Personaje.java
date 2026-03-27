098cbfc16bec389dbe05b271dd2d35ee9764b53a
roberto <robertoponcecl@gmail.com>
2026-03-27 20:36:01 +0100

    Creación de Personaje

--- practica8/src/rpg/model/Personaje.java
+++ practica8/src/rpg/model/Personaje.java
@@ -1,4 +1,69 @@
 package rpg.model;
 
 public class Personaje {
+    private int id;
+    private String nombre;
+    private int nivel;
+    private int salud;
+    private int oro;
+
+    public Personaje() {}
+
+    public Personaje(String nombre, int nivel, int salud, int oro) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.salud = salud;
+        this.oro = oro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getSalud() {
+        return salud;
+    }
+
+    public void setSalud(int salud) {
+        this.salud = salud;
+    }
+
+    public int getOro() {
+        return oro;
+    }
+
+    public void setOro(int oro) {
+        this.oro = oro;
+    }
+
+    @Override
+    public String toString() {
+        return "Personaje{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", nivel=" + nivel +
+                ", salud=" + salud +
+                ", oro=" + oro +
+                '}';
+    }
 }
