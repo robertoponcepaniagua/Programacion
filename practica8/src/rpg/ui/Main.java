12bb8c71b5a4894f41c1be94d90315c0ac6e2e79
roberto <robertoponcecl@gmail.com>
2026-03-28 08:38:08 +0100

    Conexión a la BD (funciona)

--- practica8/src/rpg/ui/Main.java
+++ practica8/src/rpg/ui/Main.java
@@ -1,4 +1,10 @@
 package rpg.ui;
 
+import rpg.utils.ConexionBD;
+
 public class Main {
+    public static void main(String [] args) throws ClassNotFoundException {
+        ConexionBD bd = new ConexionBD();
+        bd.conectar();
+    }
 }
--- practica8/src/rpg/utils/ConexionBD.java
+++ practica8/src/rpg/utils/ConexionBD.java
@@ -1,4 +1,33 @@
 package rpg.utils;
+import java.sql.*;
 
 public class ConexionBD {
+    // CÓDIGO PARA CONECTAR A LA BASE DE DATOS
+
+    // SE CONECTA MEDIANTE URL, USER, PASSWORD (LOS QUE TENGO EN entorno_bd/docker-compose.yml)
+
+    // LA URL ES LA SIGUIENTE
+    private String url = "jdbc:postgresql://localhost:5432/XRPG";
+    private String user = "xrpg_user";
+    private String password = "xrpg_password";
+
+
+    public void conectar() throws ClassNotFoundException {
+
+        try {
+            // CARGAMOS EL DRIVER
+            Class.forName("org.postgresql.Driver");
+
+            // TRY-WITH-RESOURCES (ABRE Y CIERRA SOLO)
+            try (Connection connection = DriverManager.getConnection(url, user, password)) {
+                System.out.println("Conexión");
+                // TODO: HACER UNA CONSULTA PARA PROBAR
+            }
+            // ERRORES
+        } catch (ClassNotFoundException e) {
+            System.out.println("Fallo Driver: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error de (Docker, usuario o pass): " + e.getMessage());
+        }
+    }
 }
