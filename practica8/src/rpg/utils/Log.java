5122dcefdede2d5e9bf18c59aa83b989d12d92bf
roberto <robertoponcecl@gmail.com>
2026-03-29 20:18:41 +0200

    Log terminado

--- practica8/src/rpg/utils/Log.java
+++ practica8/src/rpg/utils/Log.java
@@ -1,4 +1,40 @@
 package rpg.utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Log {
+
+    private String path;
+
+    // EL FORMATO TIENE QUE SER [FECHA] [TIPO (INFO O ERROR) ] [PAQUETE] MENSAJE
+    public Log(String path) {
+        this.path = path; //cada path es diferente así que le paso por el constructor el path
+    }
+
+
+    public void escribirFichero(String tipo, String mensaje) {
+        try (BufferedWriter file = new BufferedWriter(new FileWriter("practica7/Ficheros/errores.log", true))) {
+
+            String paquete = Thread.currentThread()
+                    .getStackTrace()[2]
+                    .getClassName();
+
+            //FUNCION: FORMATEA LA HORA PARA SER PRECIOS YA QUE AL TENER MUCHOS LOGS NO SE VE CUANDO ES DE CADA MOMENTO
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+            String fecha = LocalDateTime.now().format(formatter);
+
+            //la ciudad que la pasemos la juntamos y hacemos un ; entre cada atrinuto
+            file.write("[" + fecha + "] [" + tipo + "] [" + paquete + "] ["+ path +"] "  + mensaje);
+            //añadimos u salto de linea
+            file.newLine();
+
+        } catch (IOException e) {
+            System.out.println("No se ha podido escribir en el fichero.");
+        }
+    }
 }
