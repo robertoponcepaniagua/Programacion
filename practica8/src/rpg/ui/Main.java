2591b11f04fe23890a234471207c5eb28abbfd3a
roberto <robertoponcecl@gmail.com>
2026-03-30 10:08:35 +0200

    Menú empezado

--- practica8/src/rpg/ui/Main.java
+++ practica8/src/rpg/ui/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String [] args) throws ClassNotFoundException {
         ConexionBD bd = new ConexionBD();
         bd.conectar();
+        MenuUtils menuUtils = new MenuUtils();
+        menuUtils.menu();
     }
 }
--- practica8/src/rpg/ui/MenuUtils.java
+++ practica8/src/rpg/ui/MenuUtils.java
@@ -1,4 +1,56 @@
 package rpg.ui;
 
+import java.util.Scanner;
+
 public class MenuUtils {
+    private Scanner sc;
+
+    public MenuUtils() {
+        this.sc = new Scanner(System.in);
+    }
+
+
+    public void menu() {
+        int opcion;
+        do {
+            cabecera();
+            System.out.println(" 1. Crear Personaje");
+            System.out.println(" 2. Viajar a Ciudad");
+            System.out.println(" 3. Comprar items");
+            System.out.println(" 4. Combate PVP");
+            System.out.println(" 5. Cobro de impuestos");
+            System.out.println(" 6. Equipar Habilidad");
+            System.out.println(" 7. Estadísticas");
+            System.out.println(" 8. Ver Personajes");
+            System.out.println(" 0. Salir");
+            separador();
+            System.out.println("Elige una opción: ");
+            opcion = sc.nextInt();
+
+            // TODO: switch ()
+        } while (opcion != 0);
+    }
+
+
+    public void cabecera() {
+        System.out.println(
+                "" +
+                        "____  _______________________  ________ \n" +
+                        "\\   \\/  /\\______   \\______   \\/  _____/ \n" +
+                        " \\     /  |     ___/|       _/   \\  ___ \n" +
+                        " /     \\  |    |    |    |   \\    \\_\\  \\\n" +
+                        "/___/\\  \\ |____|    |____|_  /\\______  /\n" +
+                        "      \\_/                  \\/        \\/ "
+        );
+    }
+
+    public void separador() {
+        System.out.println();
+        System.out.println("﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌﹌");
+        System.out.println();
+    }
+
+    public void MostrarMenu() {
+
+    }
 }
