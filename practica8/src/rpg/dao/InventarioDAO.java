9b4571501a6e9f7c82197d1839a816c964bbd5d2
roberto <robertoponcecl@gmail.com>
2026-04-13 17:43:12 +0200

    Función añadida listarItemsPersonaje

--- practica8/src/rpg/dao/InventarioDAO.java
+++ practica8/src/rpg/dao/InventarioDAO.java
@@ -8,7 +8,10 @@ import rpg.utils.Log;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InventarioDAO {
     private Log log;
@@ -51,5 +54,34 @@ public class InventarioDAO {
             log.escribirFichero("ERROR","No se ha podido añadir el item " + e.getMessage());
             throw new RuntimeException("No se ha podido añadir el item " + e.getMessage());
         }
+    }
+
+    public List<Item> listarItemsPersonaje(int idPJ) {
+        String sql = "SELECT i.id, i.nombre, i.tipo, i.precio_oro, i.bonificador_ataque, i.bonificador_defensa " +
+                "FROM Items i JOIN Inventarios inv ON i.id = inv.id_item " +
+                "WHERE inv.id_personaje = ?";
+
+        List<Item> itemsPersonaje = new ArrayList<>();
+
+        try (Connection con = conexionBD.conectar();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1,idPJ);
+
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()) {
+                Item item = new Item(
+                        rs.getInt("id"),
+                        rs.getString("nombre"),
+                        rs.getString("tipo"),
+                        rs.getInt("precio_oro"),
+                        rs.getInt("bonificador_ataque"),
+                        rs.getInt("bonificador_defensa")
+                );
+                itemsPersonaje.add(item);
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return itemsPersonaje;
     }
 }
